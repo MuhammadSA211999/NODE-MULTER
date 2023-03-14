a555662a760b80659f9edeb8c3d4a996ccc86398
Muhammad SA <MuhammadSA211999>
2023-03-14 20:47:00 +0600

    upload multipleFile

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const multer = require('multer')
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.json({ name: 'NODE-MULTER APP', message: 'YOURE IN NODE-MULTER FILE UPLOADING' })
-})
-const PORT = 3000 || 5000
-app.listen(PORT, () => {
-    console.log('APP IS RUNNING');
 
+//file destination 
+const fileDest = './uploads/'
+const upload = multer({
+    dest: fileDest
+})
+
+app.get('/', (req, res) => {
+    res.send({ name: 'NODE-MULTER APP', message: 'YOURE IN NODE-MULTER FILE UPLOADING' })
+})
+app.post('/', upload.fields([{ name: 'avatar' }, { name: 'gallery', maxCount: 4 }]), (req, res) => {
+    res.send({})
+})
+const PORT = 9000
+app.listen(PORT, () => {
+    console.log(`APP IS RUNNING ON PORT ${PORT}`);
 })
